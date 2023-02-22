6517aa7235035dc9f4b381ccfc773809ecde3c31
Arris413 <91161613+Arris413@users.noreply.github.com>
2023-02-22 14:35:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(750, 600);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot() {
+    save('myFilerImage.png');
+}
